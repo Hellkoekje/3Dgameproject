265e067a1e9ed3569aa77b831618447f0d492842
sapppie <32542173+sapppie@users.noreply.github.com>
2018-10-09 23:48:06 +0200

    is een fout met het laden van de default index.html

--- CampusOfTanks/CampusOfTanks/Startup.cs
+++ CampusOfTanks/CampusOfTanks/Startup.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 
 namespace CampusOfTanks
 {
@@ -15,19 +18,19 @@ namespace CampusOfTanks
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDirectoryBrowser();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseStaticFiles(); // For the wwwroot folder
 
-            app.Run(async (context) =>
+            app.UseStaticFiles(new StaticFileOptions
             {
-                await context.Response.WriteAsync("Hello World!");
+                FileProvider = new PhysicalFileProvider(
+                    Path.Combine(Directory.GetCurrentDirectory(), "wwwroot")),
+                RequestPath = "/wwwroot"
             });
         }
     }
--- CampusOfTanks/CampusOfTanks/wwwroot/Core.js
+++ CampusOfTanks/CampusOfTanks/wwwroot/Core.js
@@ -40,5 +40,4 @@
         }
     }
     init();
-    animate();
 }
