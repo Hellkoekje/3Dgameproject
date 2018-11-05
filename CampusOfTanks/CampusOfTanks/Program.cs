4bae696fdb704d8b5c0c006bdf5fbc50f651767e
Syntasu <abouma18@hotmail.com>
2018-11-05 12:59:03 +0100

    More net stuff

--- CampusOfTanks/CampusOfTanks/Program.cs
+++ CampusOfTanks/CampusOfTanks/Program.cs
@@ -14,12 +14,6 @@ namespace CampusofTanks
 
         public static IWebHost BuildWebHost(string[] args)
         {
-            // <rant>
-            //  Build a custom webhost, to make it shut the fuck up
-            //  about files being transfered, why do you even do this ASP/Microshit?
-            //  Who the hell cares about files being transfered, UNLESS you have some issue with it...
-            //  Why is it on by default in the DefaultWebHost!?
-            // </rant>
             return new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
