e631405d4a08e5ae9d2d770acfffb1ef0c12f05d
Devihnox <32259943+Devihnox@users.noreply.github.com>
2018-11-06 16:08:13 +0100

    geluid

--- CampusOfTanks/CampusOfTanks/wwwroot/Game/Audio.js
+++ CampusOfTanks/CampusOfTanks/wwwroot/Game/Audio.js
@@ -8,7 +8,7 @@
         this.sound = new THREE.Audio(this.gameCam.cameraListener);
 
         this.audioLoader = new THREE.AudioLoader();
-        this.audioLoader.load('/sounds/Iron.mp3', (buffer) => {
+        this.audioLoader.load('/sounds/seashanty.mp3', (buffer) => {
             this.sound.setBuffer(buffer);
             this.sound.setLoop(true);
             this.sound.play();
@@ -17,8 +17,40 @@
         this.sounds.push(this.sound);
         this.setVolume(masterVolume);
 
-        this.riptank();
         
+        
+
+    }
+
+    shoot(projectile) {
+        
+        switch (projectile) {
+            case "Ei":
+                var sound1 = new THREE.PositionalAudio(this.gameCam.cameraListener);
+                this.audioLoader.load('sounds/ei.wav', function (buffer) {
+                    sound1.setBuffer(buffer);
+                    sound1.setRefDistance(20);
+                    sound1.play();
+                });
+                break;
+            case "Appel":
+                var sound1 = new THREE.PositionalAudio(this.gameCam.cameraListener);
+                this.audioLoader.load('sounds/appel.wav', function (buffer) {
+                    sound1.setBuffer(buffer);
+                    sound1.setRefDistance(20);
+                    sound1.play();
+                });
+                break;
+            case "Bier":
+                var sound1 = new THREE.PositionalAudio(this.gameCam.cameraListener);
+                this.audioLoader.load('sounds/bier.wav', function (buffer) {
+                    sound1.setBuffer(buffer);
+                    sound1.setRefDistance(20);
+                    sound1.play();
+                });
+                break;
+
+        }
 
     }
 
