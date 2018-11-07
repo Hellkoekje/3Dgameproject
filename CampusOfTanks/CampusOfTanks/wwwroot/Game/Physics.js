dc3ceb68df32059a543635819540dedf91fcc418
Syntasu <abouma18@hotmail.com>
2018-11-07 15:28:30 +0100

    Physics docs

--- CampusOfTanks/CampusOfTanks/wwwroot/Game/Physics.js
+++ CampusOfTanks/CampusOfTanks/wwwroot/Game/Physics.js
@@ -1,4 +1,5 @@
-﻿class Physics {
+﻿//Class for doing anything physics related.
+class Physics {
 
     constructor(settings) {
         this.settings = settings;
@@ -17,6 +18,12 @@
         this.physicsMaterials = [];
     }
 
+    /**
+     *  Add a new bullet to the game
+     * @param {any} mesh The bullet mesh
+     * @param {any} hitbox the bullet hitbox
+     * @param {any} body the body of the bullet.
+     */
     addBullet(mesh, hitbox, body) {
         this.world.addBody(body);
         this.bulletMeshes.push(mesh);
@@ -24,6 +31,12 @@
         
     }
 
+    /**
+     *  Add a new tank to the game
+     * @param {any} mesh The tank mesh
+     * @param {any} hitbox the tank hitbox
+     * @param {any} body the body of the tank.
+     */
     addTank(mesh, hitbox, body) {
         this.world.addBody(body);
         this.tankMeshes.push(mesh);
@@ -31,20 +44,38 @@
         
     }
 
+    /**
+     *  Add a physics body
+     * @param {any} body the body to add.
+     */
     addBody(body) {
         this.world.addBody(body);
     }
 
+    /**
+     * Add a new physics material
+     * @param {any} name name of the material
+     * @param {any} material instance of the material.
+     */
     addPhysicsMaterial(name, material) {
         this.physicsMaterials[name] = material;
         this.world.addContactMaterial(material);
     }
 
+    /**
+     * Get the physics material based on the name.
+     * @param {any} name Name of the material we want to look for.
+     * @returns {any} The material instance
+     */
     getPhysicsMaterial(name) {
         return this.physicsMaterials[name];
     }
 
+    /**
+     * Update the physics.
+     */
     update() {
+        //Step the world 1 tick forward.
         this.world.step(1 / 60);
 
         var scene = registry.components.scene;
@@ -72,10 +103,7 @@
             if (tank.alive) {
                 body.position.copy(tank.position);
                 body.quaternion.copy(tank.quaternion);
-                body.position.y += 5;
-               
-                  
-
+                body.position.y += 5;   
             }
             else {
                 this.tankMeshes.splice(n, 1);
