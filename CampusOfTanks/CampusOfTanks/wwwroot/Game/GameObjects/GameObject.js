86293d1effe19ac9df8b052ca755b73e673a5376
Syntasu <abouma18@hotmail.com>
2018-11-07 14:21:24 +0100

    Docs for GameObject and GameObjectCollection

--- CampusOfTanks/CampusOfTanks/wwwroot/Game/GameObjects/GameObject.js
+++ CampusOfTanks/CampusOfTanks/wwwroot/Game/GameObjects/GameObject.js
@@ -1,4 +1,7 @@
-﻿class GameObject extends THREE.Group {
+﻿
+//Represents a object in the game
+//  Inherits from Group, so we can place it in the scene.
+class GameObject extends THREE.Group {
 
     constructor() {
         super();
@@ -9,10 +12,19 @@
         gameObjectCollection.add(this);
     }
 
+    /**
+     * Register a method that needs to receive updates.
+     * @param {functions} callback The method (callback) we want to call when receiving an update.
+     */
     registerUpdate(callback) {
         this.updateFunctions.push(callback);
     }
 
+    /**
+     * Gets called when the main game loop updates
+     * This will update the all the registered callbacks (via register update.)
+     * @param {float} deltaTime The frame time of the current frame.
+     */
     update(deltaTime) {
         this.deltaTime = deltaTime;
         for (var i = 0; i < this.updateFunctions.length; i++) {
